32f03302d7b190e06665e2e0e61c5b0a9053489e
vagrant <Ola Daniel>
2021-12-03 21:47:37 +0000

    completed dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "images/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "images/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
 let createTweetElement = function(tweetData) {
 
   const $tweet = $(`
@@ -27,37 +52,21 @@ let createTweetElement = function(tweetData) {
   </article>
   `);
 
-  // console.log('testing call to createTweetElement');
-
   return $tweet;
 };
 
-let createTweetElementTest = function() {
+let renderTweets = function(tweets) {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton ABC",
-      "avatars": "images/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+  for (let tweet of tweets) {
+    const $element = createTweetElement(tweet);
+    $('.tweet-container').append($element);
+  }
 
-  const $tweet = createTweetElement(tweetData);
-
-  return $tweet;
 };
-
 
 $(document).ready(function() {
 
-  // console.log('client.js: page loaded!!!');
-  // console.log(createTweetElementTest());
-
-  $('.tweet-container').append(createTweetElementTest());
+  renderTweets(data);
 
 });
 
